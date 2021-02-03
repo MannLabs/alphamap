5a9967271a47855f038d5702769563ef95aa25e5
ibludau <isabell.bludau@gmail.com>
2021-02-03 18:35:27 +0100

    fix: protein sequence reference before assignment

--- pepmap/_nbdev.py
+++ pepmap/_nbdev.py
@@ -11,27 +11,28 @@ index = {"import_spectronaut_data": "Importing.ipynb",
          "get_ptm_sites": "Preprocessing.ipynb",
          "get_modifications": "Preprocessing.ipynb",
          "format_input_data": "Preprocessing.ipynb",
-         "protease_dict": "proteolytic_cleavage.ipynb",
-         "get_cleavage_sites": "proteolytic_cleavage.ipynb",
          "format_uniprot_annotation": "SequencePlot.ipynb",
          "ptm_shape_dict": "SequencePlot.ipynb",
          "get_plot_data": "SequencePlot.ipynb",
          "plot_single_peptide_traces": "SequencePlot.ipynb",
          "custom_color_palettes": "SequencePlot.ipynb",
          "uniprot_color_dict": "SequencePlot.ipynb",
+         "aa_color_dict": "SequencePlot.ipynb",
          "plot_peptide_traces": "SequencePlot.ipynb",
          "extract_note": "Uniprot_integration.ipynb",
          "extract_note_end": "Uniprot_integration.ipynb",
          "resolve_unclear_position": "Uniprot_integration.ipynb",
          "extract_positions": "Uniprot_integration.ipynb",
          "preprocess_uniprot": "Uniprot_integration.ipynb",
-         "uniprot_feature_dict": "Uniprot_integration.ipynb"}
+         "uniprot_feature_dict": "Uniprot_integration.ipynb",
+         "protease_dict": "proteolytic_cleavage.ipynb",
+         "get_cleavage_sites": "proteolytic_cleavage.ipynb"}
 
 modules = ["importing.py",
            "preprocessing.py",
-           "proteolytic_cleavage.py",
            "sequenceplot.py",
-           "uniprot_integration.py"]
+           "uniprot_integration.py",
+           "proteolytic_cleavage.py"]
 
 doc_url = "https://ibludau.github.io/pepmap/"
 
