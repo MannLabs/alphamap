ad036c65ece272b1b5c4cfe8bbc5fa7beab85ff0
voytik <jane.voytik@gmail.com>
2021-02-09 21:47:32 +0100

    feat: add new organisms to the dashboard

---
+++ alphamap/organisms_data.py
@@ -0,0 +1,41 @@
+all_organisms = {
+    'Human': {'fasta_name': 'human.fasta',
+               'uniprot_name': 'preprocessed_uniprot_human.csv'
+              }, 
+    'Mouse': {'fasta_name': 'mouse.fasta',
+               'uniprot_name': 'preprocessed_uniprot_mouse.csv'
+              }, 
+    'Rat': {'fasta_name': 'rat_[10116].fasta',
+               'uniprot_name': 'preprocessed_uniprot_rat.csv'
+              }, 
+    'Cow': {'fasta_name': 'bovine_[9913].fasta',
+               'uniprot_name': 'preprocessed_uniprot_bovine.csv'
+              }, 
+    'Zebrafish': {'fasta_name': 'zebrafish_[7955].fasta',
+               'uniprot_name': 'preprocessed_uniprot_zebrafish.csv'
+              }, 
+    'Drosophila': {'fasta_name': 'drosophila_[7227].fasta',
+               'uniprot_name': 'preprocessed_uniprot_drosophila.csv'
+              }, 
+    'Caenorhabditis elegans': {'fasta_name': 'celegans_[6239].fasta',
+               'uniprot_name': 'preprocessed_uniprot_human.csv'
+              }, 
+    'Slime mold': {'fasta_name': 'slimemold_[44689].fasta',
+               'uniprot_name': 'preprocessed_uniprot_slimemold.csv'
+              }, 
+    'Arabidopsis thaliana': {'fasta_name': 'arabidopsis_thaliana.fasta',
+               'uniprot_name': 'preprocessed_uniprot_arabidopsis.csv'
+              }, 
+    'Rice': {'fasta_name': 'rice_[39947].fasta',
+               'uniprot_name': 'preprocessed_uniprot_rice.csv'
+              }, 
+    'Escherichia coli': {'fasta_name': 'ecoli_[83333].fasta',
+               'uniprot_name': 'preprocessed_uniprot_ecoli.csv'
+              }, 
+    'Bacillus subtilis': {'fasta_name': 'bsubtilis_[224308].fasta',
+               'uniprot_name': 'preprocessed_uniprot_bsubtilis.csv'
+              }, 
+    'Saccharomyces cerevisiae': {'fasta_name': 'yeast_[559292].fasta',
+               'uniprot_name': 'preprocessed_uniprot_yeast.csv'
+              }
+}
