9f55734ab08b285219aab149fd9db4e11813a93f
Voytik <jane.voytik@gmail.com>
2021-02-10 11:07:38 +0100

    chore: add installers for Windows, Linux and MacOS

---
+++ misc/one_click_macos/create_installer_macos.sh
@@ -0,0 +1,17 @@
+rm -rf dist
+rm -rf build
+conda env remove -n alphamapinstaller
+conda create -n alphamapinstaller python=3.8 pip=20.2 -y
+# conda create -n alphamapinstaller python=3.8
+conda activate alphamapinstaller
+# call conda install git -y
+# call pip install 'git+https://github.com/MannLabs/alphamap.git#egg=alphamap[gui]' --use-feature=2020-resolver
+# brew install freetype
+pip install '../../.[gui]'
+pip install pyinstaller
+pyinstaller ../pyinstaller/alphamap.spec -y
+mv dist/alphamap dist/alphamap.app
+tar -czf dist/alphamap.app.zip dist/alphamap.app
+# chmod +x dist/alphatims.app
+# TODO No console is opened and program not blocked untill close, meaning loose threads!
+conda deactivate
