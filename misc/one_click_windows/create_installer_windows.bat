89c9b99a6e9b8f3629eea9adf8ba8a3e9d760f22
Sander Willems <swillems89@hotmail.com>
2021-04-27 15:06:23 +0200

    FIX windows installer

--- misc/one_click_windows/create_installer_windows.bat
+++ misc/one_click_windows/create_installer_windows.bat
@@ -1,33 +1,24 @@
 call rmdir dist /s /q
 call rmdir build /s /q
-call conda env remove -n alphatimsinstaller
-call conda create -n alphatimsinstaller python=3.8 -y
-REM call conda create -n alphatimsinstaller python=3.8 -y
-call conda activate alphatimsinstaller
+call conda env remove -n alphamapinstaller
+call conda create -n alphamapinstaller python=3.8 -y
+REM call conda create -n alphamapinstaller python=3.8 -y
+call conda activate alphamapinstaller
 REM call conda install git -y
-REM call pip install 'git+https://github.com/MannLabs/alphatims.git#egg=alphatims[gui]'
+REM call pip install 'git+https://github.com/MannLabs/alphamap.git#egg=alphamap[gui]'
 REM call conda install freetype
 REM call pip install ../../.[plotting]
 REM call pip install pyinstaller==4.2
-REM call pyinstaller ../pyinstaller/alphatims.spec -y
+REM call pyinstaller ../pyinstaller/alphamap.spec -y
 call cd ../..
 call rmdir dist /s /q
 call rmdir build /s /q
 call python setup.py sdist bdist_wheel
 call cd misc/one_click_windows
-call pip install "../../dist/alphatims-0.2.5-py3-none-any.whl[plotting]"
+call pip install "../../dist/alphamap-0.0.1-py3-none-any.whl"
 call pip install pyinstaller==4.2
-call pyinstaller ../pyinstaller/alphatims.spec -y
+call pyinstaller ../pyinstaller/alphamap.spec -y
 call conda deactivate
 
 
-if false; then
-  REM call mkdir inno
-  REM call pip install wget
-  REM call python -m wget https://jrsoftware.org/download.php/is.exe
-  call is.exe /SILENT /DIR=.
-  call inno\ISCC.exe alphatims_innoinstaller.iss
-else
-  call "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" alphatims_innoinstaller.iss
-  REM call iscc alphatims_innoinstaller.iss
-fi
+call "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" alphamap_innoinstaller.iss
