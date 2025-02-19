e45df1520f66a712c880840cc201f886196c3123
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-02-19 22:13:48 +0100

    fix fasta install location for windows

--- release/windows/build_installer_windows.ps1
+++ release/windows/build_installer_windows.ps1
@@ -8,9 +8,12 @@ Remove-Item -Recurse -Force -ErrorAction SilentlyContinue ./dist_pyinstaller
 $WHL_NAME = (Get-ChildItem -Path "dist" -Filter "*.whl").Name
 pip install "dist/$WHL_NAME[stable,structuremap-stable]"
 
-New-Item -ItemType Directory -Force -Path dist/alphamap_gui/alphamap/data
-Copy-Item alphamap/data/*.fasta dist/alphamap_gui/alphamap/data
-Copy-Item alphamap/data/*.csv dist/alphamap_gui/alphamap/data
-
 # Creating the stand-alone pyinstaller folder
 pyinstaller release/pyinstaller/alphamap.spec --distpath dist_pyinstaller --workpath build_pyinstaller -y
+
+New-Item -ItemType Directory -Force -Path dist_pyinstaller/alphamap/data
+Copy-Item alphamap/data/*.fasta dist_pyinstaller/alphamap/data
+Copy-Item alphamap/data/*.csv dist_pyinstaller/alphamap/data
+
+# Wrapping the pyinstaller folder in a .exe package
+&  "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" .\release\windows\alphamap_innoinstaller.iss
